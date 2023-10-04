b39191b443e8211c7053aa548a59a091c976baaa
Ricardo <ricardobolgeg@gmail.com>
2023-10-04 14:08:31 +0200

    SQL method "startConnection" made

--- src/main/java/com/RA2_Grupo2/methods/SQL_Methods.java
+++ src/main/java/com/RA2_Grupo2/methods/SQL_Methods.java
@@ -3,24 +3,26 @@ package com.RA2_Grupo2.methods;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class SQL_Methods {
 
-	static Statement statement;
-	static Connection cc;
-	private String host = "jdbc:mysql://localhost/ra2_grupo2";
-	private String user = "root";
-	private String pass = "";
+	static Connection connection;
+	private static String host="jdbc:mysql://localhost/ra2_grupo2";
+	private static String user="root";
+	private static String pass="";
 
-	public SQL_Methods() {
-		try {
-			cc = DriverManager.getConnection(host, user, pass);
-			statement = cc.createStatement();
+	public static boolean startConnection() {
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(host, user, pass);
 			System.out.println("Connection success!");
-		} catch (SQLException ex) {
-			System.out.println("Connection failed!");
+			return true;
 		}
+		catch (SQLException ex){}
+		catch (ClassNotFoundException e){}
+		System.out.println("Connection failed!");
+		return false;
 	}
 
 }
--- src/main/java/com/RA2_Grupo2/start/Test.java
+++ src/main/java/com/RA2_Grupo2/start/Test.java
@@ -1,16 +1,18 @@
 package com.RA2_Grupo2.start;
 
+import com.RA2_Grupo2.methods.SQL_Methods;
 import com.RA2_Grupo2.windows.LoginView;
 
 public class Test {
 
 	public static void main(String[] args) {
-
-		// Open the user interface
-
-		@SuppressWarnings("unused")
-		LoginView lv = new LoginView();
-
+		
+		if(SQL_Methods.startConnection())
+		{
+			// Open the user interface
+			@SuppressWarnings("unused")
+			LoginView lv = new LoginView();
+		}
 	}
 
 }
