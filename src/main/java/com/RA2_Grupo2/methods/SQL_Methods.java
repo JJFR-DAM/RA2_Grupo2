06309994ec75d1449a87472f67eac0e1ac4ff90f
Ricardo <ricardobolgeg@gmail.com>
2023-10-09 13:23:28 +0200

    Update SQL_Methods.java

--- src/main/java/com/RA2_Grupo2/methods/SQL_Methods.java
+++ src/main/java/com/RA2_Grupo2/methods/SQL_Methods.java
@@ -2,7 +2,10 @@ package com.RA2_Grupo2.methods;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
+import com.RA2_Grupo2.objects.Employee;
 
 public class SQL_Methods {
 
@@ -16,13 +19,30 @@ public class SQL_Methods {
 		{
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection(host, user, pass);
+			
 			System.out.println("Connection success!");
 			return true;
 		}
 		catch (SQLException ex){}
 		catch (ClassNotFoundException e){}
+		
 		System.out.println("Connection failed!");
 		return false;
 	}
 
+	public static void insertEmployee(Employee e) throws SQLException
+	{
+		PreparedStatement st=connection.prepareStatement(
+					"INSERT INTO employees VALUES (?,?,?,?,?,?)");
+		
+		st.setInt(1,e.getId());
+		st.setString(2,e.getNIF());
+		st.setString(3,e.getName());
+		st.setString(4,e.getSurname());
+		st.setString(5,e.getEmail());
+		st.setString(6,e.getPassword());
+		
+		st.executeUpdate();
+		st.close();
+	}
 }
